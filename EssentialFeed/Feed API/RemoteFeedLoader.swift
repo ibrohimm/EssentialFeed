b0fa3fac9841433c68b70ce5bb60ccec4ba1dd59
Ibrohim <ibrohim625m@gmail.com>
2023-01-27 18:25:40 +0500

    Delivers invalidData error on 200 HTTP response with invalid json

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public enum HTTPClientResult {
-    case success(HTTPURLResponse)
+    case success(Data, HTTPURLResponse)
     case failure(Error)
 }
 
@@ -33,7 +33,7 @@ public class RemoteFeedLoader {
     public func load(completion: @escaping (Error) -> Void) {
         client.get(from: url) { result  in
             switch result {
-            case .success(_):
+            case .success(_, _):
                 completion(.invalidData)
             case .failure(_):
                 completion(.connectivity)
