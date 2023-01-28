29d59ffb75b30cc9b512772b7f725bd29ba794ba
Ibrohim <ibrohim625m@gmail.com>
2023-01-28 14:08:51 +0500

    Move all mapping logic to the FeedItemsMapper

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -30,12 +30,7 @@ public class RemoteFeedLoader {
         client.get(from: url) { result  in
             switch result {
             case .success(let data, let response):
-                do {
-                    let items = try FeedItemsMapper.map(data, response)
-                    completion(.success(items))
-                } catch {
-                    return completion(.failure(.invalidData))
-                }
+                completion(FeedItemsMapper.map(data, from: response))
             case .failure(_):
                 completion(.failure(.connectivity))
             }
