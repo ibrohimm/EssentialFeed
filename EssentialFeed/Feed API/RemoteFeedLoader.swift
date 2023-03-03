76e00d9c2836ba91a25bd70f1d92d8077bd5f18a
Ibrohim <ibrohim625m@gmail.com>
2023-03-03 14:34:50 +0500

    Refactor `CachedFeed` type from `enum` to `tuple` since we can represent the absence of a value with an `Optional`

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -25,7 +25,7 @@ public class RemoteFeedLoader: FeedLoader {
             guard self != nil else { return }
             
             switch result {
-            case let .success(data, response):
+            case let .success((data, response)):
                 completion(RemoteFeedLoader.map(data, from: response))
             case .failure(_):
                 completion(.failure(Error.connectivity))
